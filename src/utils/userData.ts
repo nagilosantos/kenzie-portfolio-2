52dbaf54c92ca2dcb4d84e7cbf9df423d7bcd6c1
nagilosantos <nagilosantos@gmail.com>
2023-07-23 16:37:42 -0300

    Feat/ adding informations

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Nagilo Santos",
+  githubUser: "nagilosantos",
+  linkedinUser: "nagilo-santos-bb1b93199",
+  whatsappNumber: "+5571992261613",
+  emailUser: "nagilosantos@gmail.com",
+  instagramUser: "",
   facebookUser: "",
 };
